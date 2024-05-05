25b71453484458a392e3f6390b71fef1630017a9
dad2jrn <dad2jrn@gmail.com>
2024-05-05 13:35:00 -0400

    basic functionality

--- concurpy/core.py
+++ concurpy/core.py
@@ -5,8 +5,18 @@ import functools
 import logging
 
 class ConcurPy:
-    def __init__(self):
-        pass
+    def __init__(self, mode='async', workers=4, log_level='INFO'):
+        self.mode = mode
+        self.workers = workers
+        self.logger = logging.getLogger('ConcurPy')
+        self.logger.setLevel(getattr(logging, log_level.upper(), logging.INFO))
 
-    def __call__(self):
-        pass
+    def __call__(self, func):
+        if self.mode == 'async':
+            return self.async_wrapper(func)
+        elif self.mode == 'threading':
+            return self.threading_wrapper(func)
+        elif self.mode == 'multiprocessing':
+            return self.multiprocessing_wrapper(func)
+        else:
+            raise ValueError("Unsupported mode. Choose 'async', 'threading', or 'multiprocessing'.")
