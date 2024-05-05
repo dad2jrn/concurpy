15f765779665a3756e8d38789cb74697ab2f1741
dad2jrn <dad2jrn@gmail.com>
2024-05-05 14:04:21 -0400

    update core to use handleers

--- concurpy/core.py
+++ concurpy/core.py
@@ -1,8 +1,7 @@
-import asyncio
-import threading
-import multiprocessing
-import functools
 import logging
+from .async_handler import AsyncHandler
+from .threading_handler import ThreadingHandler
+from .multiprocessing_handler import MultiprocessingHandler
 
 class ConcurPy:
     def __init__(self, mode='async', workers=4, log_level='INFO'):
@@ -11,41 +10,23 @@ class ConcurPy:
         self.logger = logging.getLogger('ConcurPy')
         self.logger.setLevel(getattr(logging, log_level.upper(), logging.INFO))
 
-    def __call__(self, func):
-        if self.mode == 'async':
-            return self.async_wrapper(func)
-        elif self.mode == 'threading':
-            return self.threading_wrapper(func)
-        elif self.mode == 'multiprocessing':
-            return self.multiprocessing_wrapper(func)
+        if mode == 'async':
+            self.handler = AsyncHandler(workers)
+        elif mode == 'threading':
+            self.handler = ThreadingHandler(workers)
+        elif mode == 'multiprocessing':
+            self.handler = MultiprocessingHandler(workers)
         else:
             raise ValueError("Unsupported mode. Choose 'async', 'threading', or 'multiprocessing'.")
 
-    def async_wrapper(self, func):
-            @functools.wraps(func)
-            async def wrapped(*args, **kwargs):
-                return await asyncio.to_thread(func, *args, **kwargs)
-            return wrapped
+    def __call__(self, func):
+        if hasattr(self.handler, 'handle_task'):
+            def wrapper(*args, **kwargs):
+                return self.handler.handle_task(func, *args, **kwargs)
+            return wrapper
+        else:
+            self.logger.error(f"No handler available for {self.mode}")
+            raise NotImplementedError(f"Handler for {self.mode} is not implemented")
 
-    def threading_wrapper(self, func):
-        @functools.wraps(func)
-        def wrapped(*args, **kwargs):
-            result = None
-            def target():
-                nonlocal result
-                result = func(*args, **kwargs)
-            thread = threading.Thread(target=target)
-            thread.start()
-            thread.join()
-            return result
-        return wrapped
-
-    def multiprocessing_wrapper(self, func):
-        @functools.wraps(func)
-        def wrapped(*args, **kwargs):
-            with multiprocessing.Pool(processes=self.workers) as pool:
-                result = pool.apply(func, args=args, kwds=kwargs)
-            return result
-        return wrapped
-
-    ##TODO  possible extending this class for additional functionality
+# The ConcurPy class can now use any of the specified handlers to manage concurrency,
+# with each handler designed for a specific model (async, threading, multiprocessing).
