484717c5aa8318f967e7f38ca567f07e1dc0a556
dad2jrn <dad2jrn@gmail.com>
2024-05-05 14:20:20 -0400

    clean up and formatting

--- concurpy/core.py
+++ concurpy/core.py
@@ -1,29 +1,32 @@
-import logging
 from .utilities import setup_logging, timing, catch_exceptions
 from .async_handler import AsyncHandler
 from .threading_handler import ThreadingHandler
 from .multiprocessing_handler import MultiprocessingHandler
 
+
 class ConcurPy:
-    def __init__(self, mode='async', workers=4, log_level='INFO'):
+    def __init__(self, mode="async", workers=4, log_level="INFO"):
         setup_logging(log_level)
         self.mode = mode
         self.workers = workers
 
-        if mode == 'async':
+        if mode == "async":
             self.handler = AsyncHandler(workers)
-        elif mode == 'threading':
+        elif mode == "threading":
             self.handler = ThreadingHandler(workers)
-        elif mode == 'multiprocessing':
+        elif mode == "multiprocessing":
             self.handler = MultiprocessingHandler(workers)
         else:
-            raise ValueError("Unsupported mode. Choose 'async', 'threading', or 'multiprocessing'.")
+            raise ValueError(
+                "Unsupported mode. Choose 'async', 'threading', or 'multiprocessing'."
+            )
 
     @catch_exceptions
     @timing
     def __call__(self, func):
         def wrapper(*args, **kwargs):
             return self.handler.handle_task(func, *args, **kwargs)
+
         return wrapper
 
 
