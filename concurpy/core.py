e84a5bb340ed1e34e5abbd536b8b2eec049fb820
dad2jrn <dad2jrn@gmail.com>
2024-05-05 13:31:43 -0400

    core concurpy class

---
+++ concurpy/core.py
@@ -0,0 +1,12 @@
+import asyncio
+import threading
+import multiprocessing
+import functools
+import logging
+
+class ConcurPy:
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
